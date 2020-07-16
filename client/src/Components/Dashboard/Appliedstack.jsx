d99eb5885ffacb13d563038d871543cd38eb6c60
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-07-16 23:53:43 +0530

    New theme

---
+++ client/src/Components/Dashboard/Appliedstack.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Appliedtab from './Appliedtab'
+class Appliedstack extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        var response = [];
+        const temp =
+        {
+            position: "Position Name",
+            company: "Company Name",
+            due: new Date(),
+            category: "internship",
+            type: "react",
+            description: "datakjncd ioejd jdsckls jsdlcm jsdlc kjslkcvj jsflkkj ljsdflkcjlk kljsdckmslk jsldskld dsklfjkls klsdjflk",
+            applylink: "https://www.google.co.in/",
+            contact: "Adarsh: 97898912030",
+            furtherdetails: "details to be added mdk kjsfkl lsfkljs ksjeflksjf kljseflkjf",
+            image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Flife-style%2Fevents%2Fsurya-grahan-today-annular-solar-eclipse-2019-december-images-photos-pics-video-check-out-these-breathing-pictures-of-the-seasons-last-surya-grahan-of-26-december-2019-india%2Fphotostory%2F72975550.cms&psig=AOvVaw2iKjVJ3zl-BKVUsdd3ZT3X&ust=1594890813080000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMjB_bT1zuoCFQAAAAAdAAAAABAD"
+        };
+        for (var i = 0; i < 10; i++)
+            response.push(temp);
+        this.setState({ data: response });
+    }
+
+    render() {
+        return (
+            <div className="appliedstack-container">
+                {
+                    this.state.data.map((item, index) => {
+                        return <Appliedtab data={item} key={index} />
+                    })
+                }
+            </div>
+        );
+    }
+}
+export default Appliedstack; 
--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Opstack from './Opstack'
 import Todostack from './Todostack'
 import Opmodal from "./Opmodal"
+import Appliedstack from "./Appliedstack"
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
@@ -9,8 +10,26 @@ class Dashboard extends React.Component {
     render() {
         return (
             <div className="overlay">
-                <Opstack />
-                <Todostack />
+                <div className="first-col">
+                    <Opstack />
+                </div>
+                <div className="second-col">
+                    <div className="second-col-top">
+                    </div>
+                    <div className="second-col-bottom">
+                        <div className="second-col-bottom-stack">
+                            <Todostack />
+                        </div>
+                        <div className="second-col-bottom-stack">
+                            <Appliedstack />
+                        </div>
+                        <div className="second-col-bottom-options">
+                            <button className="second-col-bottom-buttons button1">+</button>
+                            <button className="second-col-bottom-buttons button2">-</button>
+                            <button className="second-col-bottom-buttons button3">*</button>
+                        </div>
+                    </div>
+                </div>
                 <Opmodal />
             </div>
         );
