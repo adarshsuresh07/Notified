ea70bbadff826dcc617ea0bd6eb73f54b178c2d4
haseena-hassan <haseena2199@gmail.com>
2020-07-17 15:37:30 +0530

    heroku build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require('path')
 
 
 const app = express();
@@ -24,6 +25,12 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err))
 
+  if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  }
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
