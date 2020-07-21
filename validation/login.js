7b1061a0aff3426bbed455bb5086b5e5616fcdff
haseena-hassan <haseena2199@gmail.com>
2020-07-22 01:05:42 +0530

    validation added

---
+++ validation/login.js
@@ -0,0 +1,29 @@
+const Validator = require("validator")
+const isEmpty = require("is-empty")
+
+module.exports = function validateLoginInput(data) {
+
+    let errors = {}
+    
+    data.email = !isEmpty(data.email) ? data.email : ""
+    data.password = !isEmpty(data.password) ? data.password : ""
+    
+    // Email 
+    if (Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required"
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid"
+    }
+
+
+    // Password 
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required"
+    }
+
+    
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
