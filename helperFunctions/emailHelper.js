f744df89e80af56fb1ef078b32a0518d780f4454
haseena-hassan <haseena2199@gmail.com>
2020-07-24 22:21:46 +0530

    config vars

--- helperFunctions/emailHelper.js
+++ helperFunctions/emailHelper.js
@@ -1,14 +1,16 @@
 const sgMail = require('@sendgrid/mail')
-const { sendgridKey , sendgridSender } = require('../config/keys')
-sgMail.setApiKey(sendgridKey)
+const keys = require('../config/keys')
+sgMail.setApiKey(keys.sendgridKey)
+
+
 
 
 module.exports = function sendVerificationEmail(to, token) {
     const msg = {
         to: to,
-        from: sendgridSender,
+        from: keys.sendgSender,
         subject: 'Verify Your Email',
-        text: `Click on this link to verify your email ${sendgridSender}/verification?token=${token}&email=${to}`,
+        text: `Click on this link to verify your email ${keys.sendgSender}/verification?token=${token}&email=${to}`,
         html: '<strong>Thanks for signing up to Notified</strong>',
     }
     sgMail.send(msg)
