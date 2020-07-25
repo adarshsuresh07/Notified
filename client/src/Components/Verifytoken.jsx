e2db384d8b3d6c41fd4b6782fee8752a29a9ca44
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-07-25 23:37:39 +0530

    apis alost

---
+++ client/src/Components/Verifytoken.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Verifytoken extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Verifytoken;
